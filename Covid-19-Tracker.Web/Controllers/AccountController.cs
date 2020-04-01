9b15b5c0784ec3cc1dd5db1fb5771bb5127a757f
ysf lghou <yousseflghoumaigui@gmail.com>
2020-04-01 21:30:08 +0100

    Remove unused using directives

--- Covid-19-Tracker.Web/Controllers/AccountController.cs
+++ Covid-19-Tracker.Web/Controllers/AccountController.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Covid_19_Tracker.Persistence.Data;
+﻿using System.Threading.Tasks;
 using Covid_19_Tracker.Web.Models;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
--- Covid-19-Tracker.Web/Startup.cs
+++ Covid-19-Tracker.Web/Startup.cs
@@ -1,16 +1,10 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Covid_19_Tracker.Persistence;
 using Covid_19_Tracker.Persistence.Repositories;
 using Covid_19_Tracker.Persistence.Repositories.interfaces;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Identity;
-using Microsoft.AspNetCore.Mvc.Authorization;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
